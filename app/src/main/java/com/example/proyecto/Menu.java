48e5a8db5bb008c41a86ceb47b2ec44f08a658c2
alextrones10 <alexvivanco031@gmail.com>
2020-06-03 11:48:55 -0500

    Se modificó las actividades Acerca y Recomendacioens

--- app/src/main/java/com/example/proyecto/Menu.java
+++ app/src/main/java/com/example/proyecto/Menu.java
@@ -10,6 +10,11 @@ import android.widget.Button;
 public class Menu extends AppCompatActivity {
 
     Button btnTriaje;
+    Button btnInfectados;
+    Button btnMapa;
+    Button btnAlertas;
+    Button btnRecomendaciones;
+    Button btnAcerca;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +22,51 @@ public class Menu extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         btnTriaje = (Button) findViewById(R.id.btnTriaje);
+        btnInfectados = (Button) findViewById(R.id.btnInfectados);
+        btnMapa = (Button) findViewById(R.id.btnMapa);
+        btnAlertas = (Button) findViewById(R.id.btnAlertas);
+        btnRecomendaciones = (Button) findViewById(R.id.btnRecomendaciones);
+        btnAcerca = (Button) findViewById(R.id.btnAcerca);
 
         btnTriaje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Menu.this, Triaje.class));
+
+            }
+        });
+        btnInfectados.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Infectados.class));
+
+            }
+        });
+        btnMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Mapa.class));
+
+            }
+        });
+        btnAlertas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Alertas.class));
+
+            }
+        });
+        btnRecomendaciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Recomendaciones.class));
+
+            }
+        });
+        btnAcerca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Menu.this, Acerca.class));
 
             }
         });
